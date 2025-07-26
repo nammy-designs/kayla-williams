ac1731a3ee159f29220e35c98a054fb904405bb2
nammydesigns <nammydesigns@gmail.com>
2025-07-26 16:49:26 +0530

    update investment content

--- pages/investment.tsx
+++ pages/investment.tsx
@@ -127,22 +127,51 @@ export default function Page(
         </div>
       </section>
 
-      {/* introduction */}
+      {/* the experience begins here */}
       <section
-        title="introduction"
+        title="the experience begins here"
         className="relative container/@investment w-full mx-auto before:content-[''] before:absolute before:inset-0 before:bg-[url('/images/km/9.jpeg')] before:bg-fixed before:bg-cover px-container-padding sm:px-container-padding-sm md:px-container-padding-md lg:px-container-padding-lg 2xl:px-container-padding-xl 3xl:px-container-padding-xxl my-section-padding-md lg:my-section-padding-lg 2xl:my-section-padding-2xl 3xl:my-section-padding-3xl min-h-lvh flex items-center justify-center text-center"
       >
         <div className="bg-bg-color absolute inset-0 opacity-90" aria-hidden />
         <div className="content-container relative z-[2] max-w-container-width-2xl">
-          <h2 className="text-fluid-h5 uppercase font-lora font-semibold text-primary-color pb-section-padding-sm md:pb-section-padding-md 2xl:pb-section-padding-lg">
-            introduction
-          </h2>
+          <div className="title-container pb-section-padding-sm md:pb-section-padding-md 2xl:pb-section-padding-lg">
+            <h2 className="text-fluid-h5 uppercase font-lora font-semibold text-primary-color">
+              {"The experience begins here"}
+            </h2>
+            <p className="font-medium font-lora text-fluid-body-5-guided">
+              {"Intimacy begins where the ordinary end."}
+            </p>
+          </div>
+
           <div className="text-primary-color">
             <div className="card-wrapper relative">
               <div className="content-container">
-                <p className="text-fluid-body-5-guided leading-fluid-body-4 font-medium">
-                  {`Hi Gents and Couples, My name is Kayla Williams. I am a college student and an abstract painter. In this adult-industry, I am a LOW-VOLUME elite companion. I am classy, sexy and best of all, intellectual. Science especially Genetics is my feild of interest and college study. A time spent with me is one that you will remember. Our laughs and giggles, our conversations and intimacy will be ones that I will hold dearly. I look forward to creating life-long memories of pleasure and happiness`}
+                <p className="text-fluid-body-5-guided leading-fluid-body-4">
+                  {`A bit about me...`}
                 </p>
+                <p className="text-fluid-body-5-guided leading-fluid-body-4">
+                  {`I am Kayla Williams - your private indulgence wrapped in elegance, intellect, and irresistible curves.`}
+                </p>
+                <p className="text-fluid-body-5-guided leading-fluid-body-4">
+                  {`A full time college student with a deep passion for science-genetics is my area of studies, and an abstract painter who sees beauty in everything. especially pleasure. I live with intention, and I choose to keep my circle intimate. I'm a low-volume companion because I believe real desire is cultivated, not rushed. "Refined pleasure for refined taste"`}
+                </p>
+                <p className="text-fluid-body-5-guided leading-fluid-body-4">
+                  {`With me, It's never just about the physical-it's about energy, chemistry, eye contact, and those moments that leave you thinking about me long after we part.`}
+                </p>
+                <p className="text-fluid-body-5-guided leading-fluid-body-4">
+                  <em>{`A luxury so personal it can't be repeated`}</em>
+                </p>
+                <div className="keywords-container pt-section-padding-sm md:pt-section-padding-md 2xl:pt-section-padding-lg">
+                  <span className="italic font-medium text-fluid-body-4">
+                    Discretion
+                  </span>
+                  <span className="relative italic font-medium text-fluid-body-4 before:content-['.'] after:content-['.'] before:mx-2 after:mx-2 before:text-fluid-body-3 after:text-fluid-body-3">
+                    Desire
+                  </span>
+                  <span className="italic font-medium text-fluid-body-4">
+                    Devotion
+                  </span>
+                </div>
               </div>
             </div>
           </div>
